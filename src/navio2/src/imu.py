74efce99985196af9b79bf54d4c4029479a70a56
Rishi Desai <desai.rishi1@gmail.com>
2018-04-08 13:56:43 -0500

    created imu publisher

---
+++ src/navio2/src/imu.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+import navio
+
+def imu_publisher():
+    rospy.init_node('navio2_imu')
+    pub = rospy.Publisher('lsm_imu', String)
+    rate = rospy.Rate(10)
+    imu = navio.lsm9ds1.LSM9DS1()
+    
+    while not rospy.is_shutdown()
+        m9a, m9g, m9m = imu.getMotion9();
+        data = " Gyr: {:+8.3f}".format(m9g[0]) + ", {:+8.3f}".format(m9g[1]) + ", {:+8.3f}".format(m9g[2])
+        rospy.loginfo(data)
+        pub.publish(data)
+        rate.sleep()
+
+if __name__ = 'main':
+    try:
+        imu_publisher()
+    except rospy.ROSInterruptException:
+        pass
